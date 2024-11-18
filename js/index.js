331f679e49c847e1f23a06b71bd648d80abc456d
Omonte Jeremias <jeremias150817@gmail.com>
2024-11-17 22:18:58 -0300

    Agregamos el mensaje exitoso y controlamos el mensaje enviado a formspree

--- js/index.js
+++ js/index.js
@@ -7,7 +7,13 @@ document.addEventListener('DOMContentLoaded',() => {
     const mensaje = document.querySelector('#mensaje')
     const formulario = document.querySelector('#formulario')
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const fieldset = document.querySelector('#fieldset')
 
+    const spinner = formulario.querySelector('#spinner')
+    spinner.style.display = 'none'
+    console.log(spinner.style.display)
+
+    // Objeto controlano cada entrada del formulario
     const datos = {
         nombre : '',
         apellido : '',
@@ -20,6 +26,14 @@ document.addEventListener('DOMContentLoaded',() => {
     apellido.addEventListener('input',validar); 
     correo.addEventListener('input',validar); 
     mensaje.addEventListener('input',validar); 
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(formulario); 
+
+        enviarFormulario(formData)
+        mostrarMensajeExitoso();
+    })
 
 
     // Funciones
@@ -88,7 +102,65 @@ document.addEventListener('DOMContentLoaded',() => {
         return resultado
     }
 
+    /**
+     * Asigna los valores provenientes del formulario al objeto datos
+     * @param {String} propiedad - Clave de nuestro objeto datos 
+     * @param {String} valor - Valor de nuestro objeto datos
+     */
     function ingresarDatos(propiedad,valor){
         datos[propiedad] = valor.toLowerCase(); 
+    }
+
+
+    function mostrarMensajeExitoso(){
+        
+        spinner.style.display = 'flex'
+        let mensaje = formulario.querySelector('.enviado')
+
+        if(!mensaje){
+            mensaje = document.createElement('P')
+            mensaje.classList.add('enviado')
+        }
+        mensaje.innerText = "Mensaje enviado correctamente"
+        
+        setTimeout(() => {
+            spinner.style.display = 'none'; 
+            fieldset.appendChild(mensaje)
+        }, 2000);
+        
+        setTimeout(() => {
+            mensaje.remove(); 
+        },4000)
+
+        
+        setTimeout(() => {
+            formulario.reset();
+        },2000); 
+        resetDatosFormulario();
+        btnSubmit.disabled = true; 
+    }
+
+    /**
+     * Reseteamos los datos de nuestro objeto data
+     */
+    function resetDatosFormulario(){
+        Object.keys(datos).forEach(key => datos[key] = ''); 
+    }
+
+    /**
+     * Controlamos el envio del formulario
+     * @param {Object} formData 
+     */
+    function enviarFormulario (formData){
+        fetch('https://formspree.io/f/mldrqrzr',{
+            method : 'POST',
+            body : formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Exito',data);
+        })
+        .catch(error => console.log(error))
+
     }
 } )
